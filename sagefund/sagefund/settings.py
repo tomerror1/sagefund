0ef73bbd9e05b254e30e304af3ca8ab5fed75ec3
Tom Klinge <tom.klinge@code.berlin>
2019-11-16 18:40:01 +0100

    set up postgresql

--- sagefund/sagefund/settings.py
+++ sagefund/sagefund/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'sagefund.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sagefund',
+        'USER': 'postgres',
+        'PASSWORD': 'V4l4rMorghul1s',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 

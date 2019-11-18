a8f647d05180953c6eafbc816e6354e7ac0c53e5
Tom Klinge <tom.klinge@code.berlin>
2019-11-18 18:39:05 +0100

    migrate datastructure (models) 20191118

--- sagefund/accounts/models.py
+++ sagefund/accounts/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class CustomUser(AbstractUser):
     pass
     # add additional fields in here
-    date_of_birth = models.DateField(default="1900-01-01")
+    date_of_birth = models.DateField()
 
     def __str__(self):
         return self.username
--- sagefund/app/models.py
+++ sagefund/app/models.py
@@ -1,11 +1,75 @@
 from django.db import models
 
-# Create your models here.
+from django.conf import settings
+
+class Deposit(models.Model):
+    customer = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='customer',)
+    number = models.IntegerField()
+
+    def __str__(self):
+        return self.number
+
+
+class Asset(models.Model):
+    isin = models.CharField(max_length=12)
+    wkn = models.CharField(max_length=6)
+    type_of_asset = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    current_price = models.FloatField()
+    tracking_index = models.CharField(max_length=255)
+    sustainability_raiting = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Deposit_Securities(models.Model):
+    deposit = models.OneToOneField(
+        Deposit,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    asset = models.ForeignKey(
+        Asset,
+        on_delete=models.CASCADE,
+    )
+    shares = models.FloatField()
+
+    def __str__(self):
+        return self.asset_ISIN
+
 
 class Transaction(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
     asset = models.CharField(max_length=255)
     volume =  models.IntegerField()
+    deposit = models.ForeignKey(
+        Deposit,
+        on_delete=models.CASCADE,
+    )
 
     def __str__(self):
         return self.asset
+
+
+class Portfolio(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Portfolio_securities(models.Model):
+    portfolio = models.ForeignKey(
+        Portfolio,
+        on_delete=models.CASCADE,
+    )
+    asset = models.ForeignKey(
+        Asset,
+        on_delete=models.CASCADE,
+    )
+    percentage = models.IntegerField()
+    
+    def __str__(self):
+        return self.asset
+   

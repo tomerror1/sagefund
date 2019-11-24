54499b08bc08d3db50602e842ead8ab85bf1d3a3
Tom Klinge <tom.klinge@code.berlin>
2019-11-24 18:27:12 +0100

    editing html file index.html  for implementing vue.js

---
+++ sagefund/app/templates/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <TransactionTable />
+  </div>
+</template>
+
+<script>
+// import DashBoard from './pages/DashboardPage/index'
+import TransactionTable from 'transactionPage'
+
+export default {
+  name: 'app',
+  components: {
+    TransactionTable
+  }
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
---
+++ sagefund/app/templates/transactionPage.vue
@@ -0,0 +1,66 @@
+<template lang="html">
+  <sui-table :columns="columns">
+    <sui-table-header>
+      <sui-table-row>
+        <sui-table-header-cell  v-for="(item,index) in tableHeaders"
+																v-bind:key="index">
+					{{item}}
+				</sui-table-header-cell>
+      </sui-table-row>
+    </sui-table-header>
+    <sui-table-body>
+      <sui-table-row  v-for="(row, index) in rowData" v-bind:key="index">
+        <sui-table-cell> {{ row.date }} </sui-table-cell>
+        <sui-table-cell> {{ row.asset }} </sui-table-cell>
+        <sui-table-cell> {{ row.buySell }} </sui-table-cell>
+        <sui-table-cell> {{ row.shares }} </sui-table-cell>
+        <sui-table-cell> {{ row.priceAtPurchase }} </sui-table-cell>
+				<sui-table-cell> {{ row.transactionFees }} </sui-table-cell>
+        <sui-table-cell> {{ row.valueOfTransaction }} </sui-table-cell>
+      </sui-table-row>
+    </sui-table-body>
+  </sui-table>
+</template>
+
+<script>
+export default {
+  name: "TransactionTable",
+  data: function() {
+    return {
+			tableHeaders: ['Date', 'Asset', 'Buy / Sell', 'Shares', 'Price At Purchase', 'Transaction Fees', 'Value of Transaction'],
+			rowData: [{
+				date: '3rd Nov Sun 2019',
+				asset: 'Tesla Stocks',
+				buySell: 'Buy',
+				shares: '20',
+				priceAtPurchase: '13',
+				transactionFees: '12',
+				valueOfTransaction: '40',
+			},
+			{
+				date: '27th Nov Sat 2019',
+				asset: 'SpaceX Stocks',
+				buySell: 'Sell',
+				shares: '34',
+				priceAtPurchase: '2',
+				transactionFees: '10',
+				valueOfTransaction: '45',
+			},
+			{
+				date: '13th Dec Wed 2019',
+				asset: 'Boring Co. Stocks',
+				buySell: 'Buy',
+				shares: '80',
+				priceAtPurchase: '56',
+				transactionFees: '78',
+				valueOfTransaction: '22',
+			}]
+		};
+	},
+	computed: {
+		columns: function() {
+			return this.tableHeaders.length;
+		}
+	}
+};
+</script>

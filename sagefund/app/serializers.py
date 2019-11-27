dff620499d8d14e8a9d76ebef4e1a2a9a43dbceb
Tom Klinge <tom.klinge@code.berlin>
2019-11-27 13:57:10 +0100

    creates API for KPIs

--- sagefund/app/serializers.py
+++ sagefund/app/serializers.py
@@ -1,5 +1,11 @@
 from rest_framework import serializers
-from .models import Transaction, Deposit_history, Portfolio_securities
+from .models import (
+    Transaction, 
+    Deposit_history, 
+    Portfolio_securities, 
+    Deposit_Securities,
+    Asset,
+)
 
 class TransactionSerializer(serializers.ModelSerializer):
     class Meta:
@@ -15,3 +21,14 @@ class Portfolio_securities_Serializer(serializers.ModelSerializer):
     class Meta:
         model = Portfolio_securities
         fields = '__all__'
+
+class KPI_Serializer(serializers.ModelSerializer):
+    value_of_asset = serializers.SerializerMethodField('calculate_value_of_asset')
+
+    def calculate_value_of_asset(self, Deposit_Securities):
+        value_of_asset = Deposit_Securities.shares * Deposit_Securities.asset.current_price    
+        return round(value_of_asset, 2)
+
+    class Meta:
+        model = Deposit_Securities
+        fields = ('asset', 'value_of_asset')
--- sagefund/app/viewsets.py
+++ sagefund/app/viewsets.py
@@ -1,6 +1,16 @@
 from rest_framework import viewsets
-from .models import Transaction, Deposit_history, Portfolio_securities
-from .serializers import TransactionSerializer, Deposit_history_Serializer, Portfolio_securities_Serializer
+from .models import (
+    Transaction, 
+    Deposit_history, 
+    Portfolio_securities,
+    Deposit_Securities,
+)
+from .serializers import (
+    TransactionSerializer, 
+    Deposit_history_Serializer, 
+    Portfolio_securities_Serializer, 
+    KPI_Serializer,
+)
 
 class TransactionViewSet(viewsets.ModelViewSet):
     queryset = Transaction.objects.all()
@@ -13,3 +23,7 @@ class Deposit_history_ViewSet(viewsets.ModelViewSet):
 class Portfolio_securities_ViewSet(viewsets.ModelViewSet):
     queryset = Portfolio_securities.objects.all().order_by('-percentage')
     serializer_class = Portfolio_securities_Serializer
+
+class KIP_ViewSet(viewsets.ModelViewSet):
+    queryset = Deposit_Securities.objects.all()
+    serializer_class = KPI_Serializer
